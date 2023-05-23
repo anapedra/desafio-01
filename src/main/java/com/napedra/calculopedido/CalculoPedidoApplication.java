cc0fd06adb6adf72faa5d0a1520f5eff00824040
Ana Santana <anapedra.mil@gmail.com>
2023-05-23 12:21:29 -0300

    Project created

---
+++ src/main/java/com/napedra/calculopedido/CalculoPedidoApplication.java
@@ -0,0 +1,13 @@
+package com.napedra.calculopedido;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculoPedidoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalculoPedidoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/napedra/calculopedido/CalculoPedidoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.napedra.calculopedido;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculoPedidoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
